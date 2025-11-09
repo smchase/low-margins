44d270c03c694fdca907c3f2bfa9db4576a5c111
Simon Chase <simon.chase@warp.dev>
2025-11-09 04:01:22 -0800

    a

--- simon_test/generate_test_data.py
+++ simon_test/generate_test_data.py
@@ -10,7 +10,7 @@ from camera import ROWS, COLS, COLORS
 
 def generate_test_data_json(filename: str = "test_data.json") -> None:
     test_cases = []
-    for i in range(20):
+    for i in range(100):
         data = np.random.randint(0, len(COLORS), (ROWS, COLS), dtype=np.int64)
         test_cases.append(data.tolist())
     
